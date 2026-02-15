502dcf188741ea4fe46c8b82de8b1f75047c1e3b
kirchch <kirik.osip@yandex.ru>
2026-02-15 03:52:20 +0300

    redid the code Поразрядные_операции xercise-1.c

--- Глава_1._Основы_C/Поразрядные_операции/Exercise-1.c
+++ Глава_1._Основы_C/Поразрядные_операции/Exercise-1.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 int main(void) {
-    int a = 13;
-    int bit_1 = 1, bit_2 = 2;
-    int mask = (1 << bit_1) | (1 << bit_2);
-    int result = a ^ mask;
-    printf("result: %d\n", result);
+    unsigned int a = 13;
+    unsigned int bit_1 = (a >> 1) & 1;
+    unsigned int bit_2 = (a >> 2) & 1;
+    unsigned int xor = bit_1 ^ bit_2;
+    unsigned int result = a ^ ((xor << 1) | (xor << 2));
+    printf("Result: %d\n", result);
 
     return 0;
 }
