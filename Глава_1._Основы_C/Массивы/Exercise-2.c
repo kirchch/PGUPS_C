7ed0e2fa227cf29258f00dc2905b246fadcd012d
kirchch <kirik.osip@yandex.ru>
2026-02-17 17:31:15 +0300

    выполнил упражнения 3 и 4

--- Глава_1._Основы_C/Массивы/Exercise-2.c
+++ Глава_1._Основы_C/Массивы/Exercise-2.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 int main(void) {
+    int mas[4] = {1, 2, 3, 4};
+    mas[0] *= 2;
+    mas[3] *= 2;
+    size_t count = sizeof(mas) / sizeof(mas[0]);
+    for (size_t i = 0; i < count; i++)
+    {
+        printf("%zu ", mas[i]);
+    }
     
     return 0;
 }
--- Глава_1._Основы_C/Массивы/Exercise-3.c
+++ Глава_1._Основы_C/Массивы/Exercise-3.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 int main(void) {
+    int mas[4] = {1, 2, 3, 4};
+    size_t count = sizeof(mas) / sizeof(mas[0]);
+    for (size_t i = 0; i < count; i++)
+    {
+        printf("%zu ", mas[i] * 2);
+    }
     
     return 0;
 }
