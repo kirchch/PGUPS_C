856ed1f239a254507540ca5fafc3e079db8f5378
kirchch <kirik.osip@yandex.ru>
2026-02-24 18:15:55 +0300

    Completed the tasks based on the pointers 2.1

--- Глава_2_Указатели/Константы_и_указатели/Exercise-1.c
+++ Глава_2_Указатели/Константы_и_указатели/Exercise-1.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 int main(void) {
-
+    const long a = 100;
+    const long *p = &a;
+    char *const c = "Hello";
+    printf("Constant: %ld\n", *p);
+    printf("Const pointer: %s\n", c);
     return 0;
 }
