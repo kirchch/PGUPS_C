3ef16d9adaa488eb8cb76af09eeaa9beff942a21
kirchch <kirik.osip@yandex.ru>
2026-02-24 16:49:17 +0300

    Completed the tasks based on the pointers 1.1 and 1.2

--- Глава_2_Указатели/Что_такое_указатели/Exercise-1.c
+++ Глава_2_Указатели/Что_такое_указатели/Exercise-1.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 int main(void) {
-    
+    int a = 10;
+    int *b = &a;
+    printf("Address: %p\nValue: %d\n", (void *)b, *b);
     return 0;
 }
--- Глава_2_Указатели/Что_такое_указатели/Exercise-2.c
+++ Глава_2_Указатели/Что_такое_указатели/Exercise-2.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 int main(void) {
-
+    int a = 10;
+    int *b = &a;
+    printf("Before the change: %d\n", *b);
+    *b = 74;
+    printf("After the change: %d\n", *b);
     return 0;
 }
